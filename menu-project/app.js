0ce735294fbaa9d8f17ec895c21ea8ac3358f57e
Danara Tagayeva <danara.tagayeva@gmail.com>
2022-12-06 00:20:25 -0500

    filter added

--- menu-project/app.js
+++ menu-project/app.js
@@ -82,9 +82,16 @@ const menu = [
   ];
 
   const sectionCenter=document.querySelector('.section-center');
+  const container=document.querySelector('.btn-container')
+
 
   window.addEventListener('DOMContentLoaded',function(){
-    let displayMenu=menu.map(function(item){
+   displayMenuItems(menu);  
+   displayMenuBtns();
+  })
+
+  function displayMenuItems(menuItems){
+    let displayMenu=menuItems.map(function(item){
         return `
         <article class="menu-item">
         <img src=${item.img} class="photo" alt=${item.title}/>
@@ -101,4 +108,36 @@ const menu = [
     });
     displayMenu=displayMenu.join("")
     sectionCenter.innerHTML=displayMenu;
-  })
+  }
+
+  function displayMenuBtns(){
+    const categories=menu.reduce(function(values,item){
+        if(!values.includes(item.category)){
+            values.push(item.category);
+        }
+        return values
+    
+       },['all']);
+    
+        const categoryBtns=categories.map(function(category){
+            return `<button class='filter-btn' type="button" data-id=${category}>${category}</button>`;
+        }).join("");
+        container.innerHTML=categoryBtns;
+        const filterBtns=container.querySelectorAll('.filter-btn');
+        filterBtns.forEach(function(btn){
+            btn.addEventListener('click',function(e){
+                const category=e.currentTarget.dataset.id;
+                const menuCategory=menu.filter(function(menuItem){
+                    if(menuItem.category===category){
+                        return menuItem;
+                    }  
+                });
+                if(category==='all'){
+                    displayMenuItems(menu)
+                }
+                else{
+                    displayMenuItems(menuCategory);
+                }
+            })
+          })  
+  }

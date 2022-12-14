2d94a3be069ddf2a6a9e48a4cd35eb56662372a2
Danara Tagayeva <danara.tagayeva@gmail.com>
2022-12-13 23:14:06 -0500

    date set

---
+++ countdown-project/app.js
@@ -0,0 +1,43 @@
+const months=[
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+];
+
+const weekdays=[
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+];
+
+const giveaway=document.querySelector('.giveaway');
+const deadline=document.querySelector('.deadline');
+const items=document.querySelectorAll('.deadline-format h4');
+
+let futureDate=new Date(2022,11,24,11,30,0);
+
+const year=futureDate.getFullYear();
+const hours=futureDate.getHours();
+const minutes=futureDate.getMinutes();
+
+let month=futureDate.getMonth();
+month=months[month];
+
+const date=futureDate.getDate();
+let weekday = futureDate.getDay();
+weekday=weekdays[day];
+
+giveaway.textContent=`giveaway ends on ${weekday} ${date} ${month} ${year} ${hours}:${minutes}am`;

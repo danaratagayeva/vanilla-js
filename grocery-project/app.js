45caaafaa6a5bb587ca75df59d8c693140a97165
Danara Tagayeva <danara.tagayeva@gmail.com>
2022-12-22 23:59:33 -0500

    input field set default

--- grocery-project/app.js
+++ grocery-project/app.js
@@ -67,7 +67,10 @@ function displayAlert(text,action){
 
     //set back to default
     function setBackToDefault(){
-        console.log('set back to default')
+        grocery.value='';
+        editFlag=false;
+        editId='';
+        submitBtn.textContent='submit';
     }
 
     function addToLocalStorage(id,value){
